c7f39d6277f96f39c9892ac10cd0656dce0de604
0cr0wbar0 <finnianholland2@gmail.com>
2026-02-25 16:59:39 +0000

    Added Post database schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,26 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  text,
+  integer,
+  serial,
+  pgTable,
+  varchar,
+  geometry
+} from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: varchar({ length: 255 }).notNull().unique(),
   username: varchar({ length: 20 }).notNull(),
   password_hash: varchar({ length: 60 }).notNull(),
   image_url: varchar({ length: 255 })
+});
+
+export const postTable = pgTable("Post", {
+  id: serial().primaryKey(),
+  owner_id: integer()
+    .notNull()
+    .references(() => usersTable.id),
+  description: text().notNull(),
+  route: geometry("path", { type: "Linestring", srid: 4326 }).notNull(),
+  location_name: varchar({ length: 255 }).notNull(),
+  caption: varchar({ length: 100 }).notNull()
 });
