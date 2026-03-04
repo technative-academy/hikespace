2ebe5dd34921acd4da99813b99b10c3558fd90d6
j0lol <me@j0.lol>
2026-03-04 10:20:42 +0000

    Hook up PostContent to API and style it and Main

--- frontend/src/features/post.ts
+++ frontend/src/features/post.ts
@@ -1,44 +1,25 @@
-// import useSWR from 'swr';
+import useSWR from 'swr';
 
 export type Point = [number, number];
 
+interface RouteData {
+  type: string;
+  coordinates: Point[];
+}
+
 export interface Post {
-  post_id: number;
+  id: number;
   owner_id: number;
   description: string;
-  route: Point[];
+  route: RouteData;
   location_name: string;
   caption: string;
 }
 
 export function usePost(id: number | undefined) {
-  // const { data, error, isLoading } = useSWR<Post>(
-  //   id ? `/api/posts/${id}` : null,
-  // );
+  const { data, error, isLoading } = useSWR<Post>(
+    id ? `/api/posts/${id}` : null,
+  );
 
-  const isLoading = false;
-  const error = null;
-  const data =  {
-    "post_id": id,
-    "owner_id": 1,
-    "description": "walking in Brighton!",
-    "route": [
-      [
-        50.828873,
-        -0.141176,
-      ],
-      [
-        50.8293,
-        -0.1422,
-      ],
-      [
-        50.8302,
-        -0.1431,
-      ]
-    ],
-    "location_name": "Brighton",
-    "caption": "hi, this is a post about walking in Brighton!"
-  };
-
-  return { post: data as Post, isLoading, error: error };
+  return { post: data as Post | undefined, isLoading, error: error };
 }
