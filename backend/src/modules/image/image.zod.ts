ec0e64c5a8aac4371bb8488e1c9b2341f2bdd716
0cr0wbar0 <finnianholland2@gmail.com>
2026-03-04 15:46:03 +0000

    Added Backblaze functionality

--- backend/src/modules/image/image.zod.ts
+++ backend/src/modules/image/image.zod.ts
@@ -4,7 +4,14 @@ import { imageTable } from "../../db/schema.js";
 
 export const ImageSchema = createSelectSchema(imageTable);
 
-export const CreateImageSchema = createInsertSchema(imageTable);
+export const uploadImageMetadataSchema = z.object({
+  post_id: z.number().int(),
+  metadata: z.array(
+    z.object({
+      position: z.number().int()
+    })
+  )
+});
 
 export type Image = z.infer<typeof ImageSchema>;
-export type CreateImageDto = z.infer<typeof CreateImageSchema>;
+export type CreateImageMetadataDto = z.infer<typeof uploadImageMetadataSchema>;
