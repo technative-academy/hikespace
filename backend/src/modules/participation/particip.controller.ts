48b247686ebe249ff505f24dc443988634cb4a88
Kostiantyn Semenenko <major7577@gmail.com>
2026-03-11 11:34:52 +0000

    Added arrays logic

--- backend/src/modules/participation/particip.controller.ts
+++ backend/src/modules/participation/particip.controller.ts
@@ -1,13 +1,16 @@
 import type { Request, Response } from "express";
 import type { ParticipService } from "./particip.service.js";
-import { createParticipSchema } from "./particip.zod.js";
+import {
+  createManyParticipSchema,
+  createParticipSchema
+} from "./particip.zod.js";
 import { IdParamSchema } from "#utils/validators.js";
 
 export class ParticipController {
   constructor(private readonly participService: ParticipService) {}
 
   create = async (req: Request, res: Response) => {
-    const parsed = createParticipSchema.safeParse(req.params);
+    const parsed = createParticipSchema.safeParse(req.body);
 
     if (!parsed.success) {
       return res
@@ -17,6 +20,26 @@ export class ParticipController {
 
     try {
       const particip = await this.participService.create(parsed.data);
+
+      return res.status(201).json(particip);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "Failed to create participation" });
+    }
+  };
+
+  createMany = async (req: Request, res: Response) => {
+    const parsed = createManyParticipSchema.safeParse(req.body);
+
+    if (!parsed.success) {
+      return res
+        .status(400)
+        .json({ message: "Invalid request params", errors: parsed.error });
+    }
+
+    try {
+      const particip = await this.participService.createMany(parsed.data);
 
       return res.status(201).json(particip);
     } catch (err) {
--- backend/src/modules/participation/particip.service.ts
+++ backend/src/modules/participation/particip.service.ts
@@ -1,17 +1,35 @@
-import type { ParticipRepository } from "./particip.repository.js";
-import type { CreateParticipDto } from "./particip.zod.js";
+import { PostRepository } from "#modules/post/post.repository.js";
+import type { Particip, ParticipRepository } from "./particip.repository.js";
+import type {
+  CreateManyParticipDto,
+  CreateParticipDto
+} from "./particip.zod.js";
 
 export class ParticipService {
-  constructor(private readonly particips: ParticipRepository) {}
+  constructor(
+    private readonly particips: ParticipRepository,
+    posts: PostRepository
+  ) {}
 
-  async create(dto: CreateParticipDto) {
+  async create(dto: CreateParticipDto, owner_id: string): Promise<Particip> {
     return this.particips.create({
       user_id: dto.user_id,
       post_id: dto.post_id
     });
   }
 
-  async delete(id: number): Promise<void> {
+  async createMany(
+    dto: CreateManyParticipDto,
+    owner_id: string
+  ): Promise<Particip[]> {
+    return this.particips.createMany(dto.postId, dto.userIds);
+  }
+
+  async delete(id: number, owner_id: string): Promise<void> {
     return this.particips.delete(id);
+  }
+
+  async isOwner(post_id: number, owner_id: string): Promise<boolean> {
+    return false;
   }
 }
